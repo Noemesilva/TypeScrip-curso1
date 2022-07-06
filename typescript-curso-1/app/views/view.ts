25e55899d2741389691f34da08ee68f7b78e9bb4
Noeme Silva <93559668+Noemesilva@users.noreply.github.com>
2022-07-06 15:26:40 -0300

    projeto substituido pelo projeto do professor

--- typescript-curso-1/app/views/view.ts
+++
@@ -1,7 +0,0 @@
-export class View {
-    protected elemento: HTMLElement;// ( protected )  todo mundo que herdar vai ter acesso
-
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
-}
