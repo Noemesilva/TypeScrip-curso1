d9f00277832e3a13020eef19e924706be67f19e0
Noeme Silva <93559668+Noemesilva@users.noreply.github.com>
2022-07-07 15:59:19 -0300

    Evitando importar negociações duplicadas

---
+++ typescript-curso-1/app/src/interfaces/negociacao-do-dia.ts
@@ -0,0 +1,5 @@
+ export interface NegociacaoesDodia {
+    montante: number;
+    vezes: number;
+
+}
--- typescript-curso-1/app/src/models/negociacao.ts
+++ typescript-curso-1/app/src/models/negociacao.ts
@@ -1,19 +1,12 @@
-export class Negociacao {
+import { Imprimivel } from "../utils/imprimivel.js";
+
+export class Negociacao implements Imprimivel{
     constructor(
         private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
-
-    get volume(): number {
-        return this.quantidade * this.valor;
-    }
-
-    get data(): Date {
-        const data = new Date(this._data.getTime());
-        return data;
-    }
-
+    
     public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
         const exp = /-/g;
         const date = new Date(dataString.replace(exp, ','));
@@ -21,4 +14,27 @@ export class Negociacao {
         const valor = parseFloat(valorString);
         return new Negociacao(date, quantidade, valor);
     }
+    get volume(): number {
+        return this.quantidade * this.valor;
+    }
+    
+    get data(): Date {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+    
+    public paraTexto(): string{
+        return `Data: ${this.data},
+        Quantidade: ${this.quantidade},
+        Valor: ${this.valor}
+        `;
+    }
+
+    public ehIgual(Negociacao: Negociacao): boolean                           {
+        
+        return this.data.getDate() === Negociacao.data.getDate() 
+        && this.data.getMonth() === Negociacao.data.getMonth() 
+        && this.data.getFullYear() === Negociacao.data.getFullYear();
+    }
+    
 }
